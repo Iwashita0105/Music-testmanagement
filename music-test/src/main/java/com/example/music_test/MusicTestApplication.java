0145eee9e639ae75ad2b05621c516ca99d1ca33b
Iwashita Ibuki <Iwashita.Ibuki@technopro.com>
2025-12-22 09:56:16 +0900

    spring追加

---
+++ music-test/src/main/java/com/example/music_test/MusicTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.music_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicTestApplication.class, args);
+	}
+
+}
